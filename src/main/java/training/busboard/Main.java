645ab2055b67f2f19f2a75b091802a85dc74c25b
Leonardo Phithak <leonardo.phithak@gmail.com>
2024-09-18 11:05:41 +0100

    Removed dependencies and used tfl java request code

--- src/main/java/training/busboard/Main.java
+++ src/main/java/training/busboard/Main.java
@@ -1,7 +1,40 @@
-package training.busboard;
+package training.busboard; 
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Main {
-    public static void main(String args[]) {
-        // Your code here!
+
+    public static void main(String[] args) {
+        try {
+            String urlString = "https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals";
+            URL url = new URL(urlString);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            //Request headers
+            connection.setRequestProperty("Cache-Control", "no-cache");
+
+            connection.setRequestMethod("GET");
+
+            int status = connection.getResponseCode();
+            System.out.println(status);
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream())
+            );
+            String inputLine;
+            StringBuffer content = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            in.close();
+            System.out.println(content);
+
+            connection.disconnect();
+        } catch (Exception ex) {
+            System.out.print("exception:" + ex.getMessage());
+        }
     }
-}	
+}
